bae1974158fbb96085fd345f6fb660fdce67fe1c
LoreMarti91 <martignettilorenzo@gmail.com>
2026-05-13 18:36:24 +0200

    Update Log interface

--- src/pi3hat/pi3hat_hw_interface/src/actuator_manager.cpp
+++ src/pi3hat/pi3hat_hw_interface/src/actuator_manager.cpp
@@ -183,6 +183,12 @@ bool Actuator_Manager::ConfigureActuator(std::shared_ptr<mjbots::moteus::Transpo
     cmd_diagn = "conf set servo.max_velocity " + cmd_str + "\n";
     SendDiagnosticCommandAndVerify(cmd_diagn);
 
+    cmd_diagn = "conf set servo.max_voltage " + std::to_string(act_opt_.max_voltage) + "\n";
+    SendDiagnosticCommandAndVerify(cmd_diagn);
+    
+    cmd_diagn = "conf set servo.flux_brake_margin_voltage " + std::to_string(act_opt_.flux_brake_margin_voltage) + "\n";
+    SendDiagnosticCommandAndVerify(cmd_diagn);
+    
     cmd_diagn = "conf set servo.max_power_W " + std::to_string(act_opt_.max_power_W) + "\n";
     SendDiagnosticCommandAndVerify(cmd_diagn);
     
